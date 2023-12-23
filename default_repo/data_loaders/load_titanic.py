34da4abbbca497f54d25ce52daa5c66db966c73b
eponkratova <kate.ponkratova@gmail.com>
2023-12-23 11:36:48 +0000

    Test

---
+++ default_repo/data_loaders/load_titanic.py
@@ -0,0 +1,20 @@
+import io
+import pandas as pd
+import requests
+from pandas import DataFrame
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(**kwargs) -> DataFrame:
+    """
+    Template for loading data from API
+    """
+    url = 'https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv?raw=True'
+
+    return pd.read_csv(url)
+
